08dd21cc7832c8f1357b225167f844538d6754bf
R2tex <nico1ereclase@gmail.com>
2026-02-05 12:09:21 +0100

    croco cluster

--- src/main/java/fr/pgah/Crocodile.java
+++ src/main/java/fr/pgah/Crocodile.java
@@ -9,29 +9,10 @@ import java.awt.*;
 public class Crocodile extends Bestiole {
 
   public Action getAction(BestioleInfo info) {
-    // Infecte si un ennemi est en face
     if (info.getEnFace() == Voisin.AUTRE) {
-      return Action.INFECTER;
-    }
-    // Si un autre crocodile est à côté, avance pour rester avec le groupe
-    else if (info.getADroite() == Voisin.MEME || info.getAGauche() == Voisin.MEME) {
-      if (info.getEnFace() == Voisin.RIEN) {
-        return Action.SAUTER;  // Avance le long du groupe
-      } else {
-        return Action.DROITE;  // Tourne pour longer le groupe
-      }
-    }
-    // Si un mur est en face, tourne
-    else if (info.getEnFace() == Voisin.MUR) {
-      return Action.GAUCHE;
-    }
-    // Si un autre crocodile est en face, tourne pour le contourner
-    else if (info.getEnFace() == Voisin.MEME) {
-      return Action.DROITE;
-    }
-    // Sinon avance
-    else {
-      return Action.SAUTER;
+      return Action.INFECTER;  // Infecte s'il y a une autre bestiole en face
+    } else {
+      return Action.GAUCHE;  // Tourne à gauche sinon
     }
   }
   
